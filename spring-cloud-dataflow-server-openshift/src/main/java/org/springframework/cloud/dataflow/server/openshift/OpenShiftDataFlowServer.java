a87e1c3845ec03676186e09f351a44733bb7ed1c
Donovan Muller <donovan.muller@gmail.com>
2017-03-28 05:28:33 +0200

    Bumped to new development version

--- spring-cloud-dataflow-server-openshift/src/main/java/org/springframework/cloud/dataflow/server/openshift/OpenShiftDataFlowServer.java
+++ spring-cloud-dataflow-server-openshift/src/main/java/org/springframework/cloud/dataflow/server/openshift/OpenShiftDataFlowServer.java
@@ -1,6 +1,7 @@
 package org.springframework.cloud.dataflow.server.openshift;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
 
 /**
@@ -8,6 +9,7 @@ import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
  *
  * @author Donovan Muller
  */
+@SpringBootApplication
 @EnableDataFlowServer
 public class OpenShiftDataFlowServer {
 
