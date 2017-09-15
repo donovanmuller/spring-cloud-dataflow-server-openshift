6fc168644e0e787a263ebd2f163dde7cd8e4a823
Donovan Muller <donovan.muller@gmail.com>
2017-09-15 16:43:20 +0200

    Updated for latest versions

--- src/etc/openshift/pull-images.sh
+++ src/etc/openshift/pull-images.sh
@@ -5,10 +5,10 @@ echo "Pulling images..."
 declare -a images=(
   "mysql:5.6"
   "redis:3-alpine"
-  "donovanmuller/spring-cloud-dataflow-server-openshift:1.1.0.RELEASE"
+  "donovanmuller/spring-cloud-dataflow-server-openshift:1.2.0.RELEASE"
   "rabbitmq:3-management"
   "digitalwonderland/zookeeper"
-  "wurstmeister/kafka:0.10.1.0"
+  "wurstmeister/kafka:0.10.2.1"
   )
 
 for((i=0;i<${#images[@]};i++))
